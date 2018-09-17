0c0d62ddc4400e6a2cf4a807e47dec7de603d473
Chery Vang <misscheryvang@yahoo.com>
2018-09-17 00:01:37 -0500

    fixed note input box.

--- server.js
+++ server.js
@@ -79,12 +79,12 @@ app.get("/articles", function (req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
-  db.Article.find(
+  db.Article.findOne(
     {
       _id: (req.params.id)
     },
   )
-    .populate(note)
+    .populate("note")
     .then(function (dbArticle) {
       // If all Notes are successfully found, send them back to the client
       res.json(dbArticle);
@@ -96,21 +96,21 @@ app.get("/articles/:id", function (req, res) {
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function (req, res) {
-  db.Article.Update(
-    {
-      _id: (req.params.id)
-    },
-    {
-      $push: { notes: dbNote._id }
-    },
-  )
-    .then(function (dbArticle) {
-      // If all Notes are successfully found, send them back to the client
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function (err) {
-      // If an error occurs, send the error back to the client
+    .catch(function(err) {
+      // If an error occurred, send it to the client
       res.json(err);
     });
 });
